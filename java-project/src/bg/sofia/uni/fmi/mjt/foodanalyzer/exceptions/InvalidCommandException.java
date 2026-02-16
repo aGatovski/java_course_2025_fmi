57860dbc20531aac86b4f27aee1049cc0e230f9f
aGatovski <145013465+aGatovski@users.noreply.github.com>
2026-02-16 14:58:12 +0200

    Line fixes

--- java-project/src/bg/sofia/uni/fmi/mjt/foodanalyzer/exceptions/InvalidCommandException.java
+++ java-project/src/bg/sofia/uni/fmi/mjt/foodanalyzer/exceptions/InvalidCommandException.java
@@ -1,6 +1,5 @@
 package bg.sofia.uni.fmi.mjt.foodanalyzer.exceptions;
 
-//trq vidq checked ili unchecked mislq checked i  da go handle kato catch kaji neshto??? ne da krashva
 public class InvalidCommandException extends Exception {
     public InvalidCommandException(String message) {
         super(message);
--- java-project/src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/api/FoodDataApiClient.java
+++ java-project/src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/api/FoodDataApiClient.java
@@ -98,4 +98,3 @@ public class FoodDataApiClient {
         }
     }
 }
-//FINITO TEST IT
