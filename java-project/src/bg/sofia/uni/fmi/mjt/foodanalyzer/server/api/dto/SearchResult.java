880d328d278d829fe27ae5844e52c157d32736e3
aGatovski <145013465+aGatovski@users.noreply.github.com>
2026-03-15 12:34:13 +0200

    Delete java-project directory

--- java-project/src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/api/dto/SearchResult.java
+++
@@ -1,6 +0,0 @@
-package bg.sofia.uni.fmi.mjt.foodanalyzer.server.api.dto;
-
-import java.util.List;
-
-public record SearchResult(int totalHits, int currentPage, int totalPages, List<SearchResultFood> foods) {
-}
--- java-project/src/bg/sofia/uni/fmi/mjt/foodanalyzer/server/command/Command.java
+++
@@ -1,5 +0,0 @@
-package bg.sofia.uni.fmi.mjt.foodanalyzer.server.command;
-
-public interface Command {
-    String execute();
-}
